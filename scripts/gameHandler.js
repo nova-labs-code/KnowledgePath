949b27039e3c486ef3a50585e14e6017b7375043
nova-labs-code <mt94ym5qjv@privaterelay.appleid.com>
2026-02-05 18:52:11 -0500

    Update gameHandler.js

--- scripts/gameHandler.js
+++ scripts/gameHandler.js
@@ -3,4 +3,6 @@ function launchGame(grade, subject, game) {
   const bar = document.getElementById("progress-bar");
   const width = Math.floor(Math.random() * 100);
   bar.style.width = width + "%";
+
+  // Future: replace alert with actual interactive games
 }
